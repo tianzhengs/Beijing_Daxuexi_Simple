8d055f8ebf73efcbef1855afec5f0446230b8079
starkkkk <stzz2001@gmail.com>
2021-11-15 21:17:33 +0800

    Initial commit
    Initial commit

---
+++ main.py
@@ -0,0 +1,99 @@
+import json
+import os
+import re
+import traceback
+from base64 import b64encode
+
+import requests
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pksc1_v1_5
+from Crypto.PublicKey import RSA
+from ddddocr import DdddOcr
+
+org_id = '172442'  # "北京市海淀团区委"
+
+username = os.environ["USERNAME"]
+password = os.environ["PASSWORD"]
+
+def encrypt(t):
+    public_key = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQD5uIDebA2qU746e/NVPiQSBA0Q3J8/G23zfrwMz4qoip1vuKaVZykuMtsAkCJFZhEcmuaOVl8nAor7cz/KZe8ZCNInbXp2kUQNjJiOPwEhkGiVvxvU5V5vCK4mzGZhhawF5cI/pw2GJDSKbXK05YHXVtOAmg17zB1iJf+ie28TbwIDAQAB\n-----END PUBLIC KEY-----"
+    rsa_key = RSA.importKey(public_key)
+    cipher = Cipher_pksc1_v1_5.new(rsa_key)
+    cipher_text = b64encode(cipher.encrypt(t.encode()))
+    return cipher_text.decode()
+
+
+bjySession = requests.session()
+# set session timeout
+bjySession.timeout = 5
+fake_ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36 Edg/80.0.361.111'
+bjySession.headers.update({
+    "User-Agent": fake_ua, })
+
+url = ''
+for _ in range(3):
+    try:
+        r = bjySession.get(url="https://m.bjyouth.net/site/login")
+        cap_url = "https://m.bjyouth.net" + re.findall(
+            r'src="/site/captcha.+" alt=', r.text)[0][5:-6]
+
+        cap = bjySession.get(url=cap_url)
+        ocr = DdddOcr()
+        cap_text = ocr.classification(cap.content)
+        print(f'Captcha OCR: {cap_text}')
+        _csrf_mobile = bjySession.cookies.get_dict()['_csrf_mobile']
+        # TODO: 有时间看一下这个算法
+        login_password = encrypt(password)
+        login_username = encrypt(username)
+        login_r = bjySession.post('https://m.bjyouth.net/site/login',
+                                  data={
+                                      '_csrf_mobile': _csrf_mobile,
+                                      'Login[username]': login_username,
+                                      'Login[password]': login_password,
+                                      'Login[verifyCode]': cap_text
+                                  })
+        print(f'Login: {login_r.text}')
+        r = json.loads(bjySession.get("https://m.bjyouth.net/dxx/index").text)
+        if 'newCourse' not in r:
+            print(r)
+        url = r['newCourse']['url']
+        title = r['newCourse']['title']
+        break
+    except:
+        print(traceback.format_exc())
+
+if not url:
+    print('Fail in login')
+    exit(1)
+
+r2 = bjySession.get('https://m.bjyouth.net/dxx/my-integral?type=2&page=1&limit=15')
+res = json.loads(r2.text)
+if f"学习课程：《{title}》" in list(map(lambda x: x['text'], res['data'])):
+    print(f'{title} already finished')
+    exit(0)
+
+pattern = re.compile(r'https://h5.cyol.com/special/daxuexi/(\w+)/m.html\?t=1&z=201')
+result = pattern.search(url)
+if not result:
+    print(f'Url pattern not matched: {url}')
+    exit(1)
+
+end_img_url = f'https://h5.cyol.com/special/daxuexi/{result.group(1)}/images/end.jpg'
+study_url = f"https://m.bjyouth.net/dxx/check?id={r['newCourse']['id']}&org_id={org_id}"
+
+r = bjySession.get(study_url)
+
+if r.text:
+    print(
+        f'Unexpected response: {r.text}'
+    )
+    exit(1)
+
+r = bjySession.get('https://m.bjyouth.net/dxx/my-integral?type=2&page=1&limit=15')
+res = json.loads(r.text)
+if f"学习课程：《{title}》" in list(map(lambda x: x['text'], res['data'])):
+    print(f'{title} finished')
+    exit(0)
+else:
+    print(f'Seem finished {title}, but not confirmed as {f"学习课程：《{title}》"}' not in {
+        list(map(lambda x: x['text'], res['data']))})
+    exit(1)
---
+++ readme.md
@@ -0,0 +1,21 @@
+# 北京青年大学习
+基于Github Action运行,简洁版本
+
+在运行时获取最新一集,如未学习进行学习,已学习则结束
+
+建议配置运行频率一周2次(默认为3天一次),没有成功会出错,默认配置下Github会向邮箱推送,所以没有推送功能
+
+(组织不是海淀区团委可能需要修改org_id)
+
+感谢[ouyen/qndxx-beijing](https://github.com/ouyen/qndxx-beijing)的启发,因为按自己风格改了很多东西,所以没有放在Fork中
+
+# 配置 Secret
+填写以下SECRET （名称均为大写）: 
+
+(账号密码为登录青春北京的信息,可以在[这里](https://m.bjyouth.net/site/login)测试登录信息) 
+
+​	USERNAME: 账号  
+
+​	PASSWORD: 密码  
+
+[如何添加SECRET](https://docs.github.com/cn/actions/security-guides/encrypted-secrets#creating-encrypted-secrets-for-a-repository)
